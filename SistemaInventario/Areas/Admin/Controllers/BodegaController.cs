c28f3cb61c7ccc2ef72aa88a1cbcb793b80f2646
Cristianss2811 <cristianss.2811@gmail.com>
2024-03-18 09:38:28 -0600

    Creacion de Vistas parciales Crear y Modificar Bodegas

--- SistemaInventario/Areas/Admin/Controllers/BodegaController.cs
+++ SistemaInventario/Areas/Admin/Controllers/BodegaController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos;
 
 namespace SistemaInventario.Areas.Admin.Controllers
 {
@@ -15,6 +16,24 @@ namespace SistemaInventario.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        //metodo Upsert GET
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            Bodega bodega = new Bodega();
+            if (id == null) 
+            {
+                //creamos un nuevo registro
+                bodega.Estado = true;
+                return View(bodega);
+            }
+            bodega = await _unidadTrabajo.Bodega.Obtener(id.GetValueOrDefault());
+            if(bodega == null) 
+            { 
+                return NotFound();
+            }
+            return View(bodega);
         }
 
         #region API
