a4ae76abb0205f472c40f293f6c4f957f2188d20
Cristianss2811 <cristianss.2811@gmail.com>
2024-04-25 23:10:27 -0600

    Crud Productos Terminado

--- SistemaInventario/Areas/Admin/Controllers/ProductoController.cs
+++ SistemaInventario/Areas/Admin/Controllers/ProductoController.cs
@@ -4,6 +4,7 @@ using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
 using SistemaInventario.Modelos;
 using SistemaInventario.Modelos.ViewModels;
 using SistemaInventario.Utilidades;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Drawing;
 
@@ -13,10 +14,12 @@ namespace SistemaInventario.Areas.Admin.Controllers
     public class ProductoController : Controller
     {
         private readonly IUnidadTrabajo _unidadTrabajo;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductoController(IUnidadTrabajo unidadTrabajo)
+        public ProductoController(IUnidadTrabajo unidadTrabajo,IWebHostEnvironment webHostEnvironment)
         {
             _unidadTrabajo = unidadTrabajo;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -31,7 +34,8 @@ namespace SistemaInventario.Areas.Admin.Controllers
             {
                 Producto = new Producto(),
                 CategoriaLista = _unidadTrabajo.Producto.ObtenerTodosDropDownList("Categoria"),
-                MarcaLista = _unidadTrabajo.Producto.ObtenerTodosDropDownList("Marca")
+                MarcaLista = _unidadTrabajo.Producto.ObtenerTodosDropDownList("Marca"),
+                PadreLista = _unidadTrabajo.Producto.ObtenerTodosDropDownList("Producto")
             };
 
             if(id == null)
@@ -52,9 +56,76 @@ namespace SistemaInventario.Areas.Admin.Controllers
         }
 
 
-       
+
 
         #region API
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>Upsert(ProductoVM productoVM)
+        {
+            var files = HttpContext.Request.Form.Files;
+            string webRootPath = _webHostEnvironment.WebRootPath;
+            if (ModelState.IsValid)
+            {
+                if (productoVM.Producto.Id == 0)
+                {
+                    //crear un nuevo producto
+                    string upload = webRootPath + DS.ImagenRuta;
+                    //crear un id unico en mi sistema 
+                    string fileName = Guid.NewGuid().ToString();
+                    //creamos una variable para conocer la extensión del archivo
+                    string extension = Path.GetExtension(files[0].FileName);
+                    //habilitar el filestream para crear el archivo de imagen en tiempo real
+                    using (var filestream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(filestream);
+                    }
+                    productoVM.Producto.ImagenUrl = fileName + extension;
+                    await _unidadTrabajo.Producto.Agregar(productoVM.Producto);
+                }
+                else
+                {
+                    //Actualizar al producto
+                    var objProducto = await _unidadTrabajo.Producto.ObtenerPrimero(p => p.Id == productoVM.Producto.Id, isTracking: false);
+                    if (files.Count > 0)
+                    {
+                        string upload = webRootPath + DS.ImagenRuta;
+                        string fileName = Guid.NewGuid().ToString();
+                        string extension = Path.GetExtension(files[0].FileName);
+
+                        //borrar la imagen anterior
+                        var anteriorFile = Path.Combine(upload, objProducto.ImagenUrl);
+                        //Verificamos que la imagen exista
+                        if (System.IO.File.Exists(anteriorFile))
+                        {
+                            System.IO.File.Delete(anteriorFile);
+                        }
+                        //creamos la nueva imagen
+                        using (var filestream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                        {
+                            files[0].CopyTo(filestream);
+                        }
+                        productoVM.Producto.ImagenUrl = fileName + extension;
+                    }//si no elige imagen
+                    else
+                    {
+                        productoVM.Producto.ImagenUrl = objProducto.ImagenUrl;
+                    }
+                    _unidadTrabajo.Producto.Actualizar(productoVM.Producto);
+                }
+                TempData[DS.Exitosa] = "Producto Registrado";
+                await _unidadTrabajo.Guardar();
+                return View("Index");
+            }
+            productoVM.CategoriaLista = _unidadTrabajo.Producto.ObtenerTodosDropDownList("Categoria");
+            productoVM.MarcaLista = _unidadTrabajo.Producto.ObtenerTodosDropDownList("Marca");
+            productoVM.PadreLista = _unidadTrabajo.Producto.ObtenerTodosDropDownList("Producto");
+            return View(productoVM);
+
+        }
+
+
         [HttpGet]
         public async Task<IActionResult> ObtenerTodos()
         {
@@ -93,8 +164,17 @@ namespace SistemaInventario.Areas.Admin.Controllers
             var productoDB = await _unidadTrabajo.Producto.Obtener(id);
             if (productoDB == null)
             {
-                return Json(new { success = false, message = "Error al borrar el rgistro en la Base de datos" });
+                return Json(new { success = false, message = "Error al borrar el registro en la Base de datos" });
             }
+            //borramos la imagen del producto eliminado
+            string upload = _webHostEnvironment.WebRootPath + DS.ImagenRuta;
+            var anteriorFile = Path.Combine(upload, productoDB.ImagenUrl);
+            if(System.IO.File.Exists(anteriorFile))
+            {
+                System.IO.File.Delete(anteriorFile);
+            }
+
+
             _unidadTrabajo.Producto.Remover(productoDB);
             await _unidadTrabajo.Guardar();
             return Json(new { success = true, message = "Producto eliminado con exito" });
